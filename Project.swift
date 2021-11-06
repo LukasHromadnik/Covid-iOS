11f6d4e970dcac01ded72f58072fa24a230e2abb
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2021-11-06 09:26:29 +0100

    Update tuist setup

--- Project.swift
+++ Project.swift
@@ -3,38 +3,65 @@ import ProjectDescriptionHelpers
 import CodeSigning
 
 let bundleIdentifier = "cz.ackee.enterprise.covid"
+let deploymentTarget = DeploymentTarget.iOS(targetVersion: "15.0", devices: .iphone)
 
 let core = Target(
     name: "CovidCore",
     platform: .iOS,
     product: .framework,
     bundleId: bundleIdentifier + ".core",
+    deploymentTarget: deploymentTarget,
     infoPlist: .default,
     sources: "Features/CovidCore/**"
 )
 
-let bars = Target.graph(
+let bars = Target.feature(
     name: "Bars",
-    bundleIdentifier: bundleIdentifier + ".bars"
+    bundleIdentifier: bundleIdentifier + ".bars",
+    deploymentTarget: deploymentTarget
 )
 
-let numberR = Target.graph(
+let numberR = Target.feature(
     name: "NumberR",
-    bundleIdentifier: bundleIdentifier + ".numberr"
+    bundleIdentifier: bundleIdentifier + ".numberr",
+    deploymentTarget: deploymentTarget
 )
 
-let dailyReport = Target.graph(
+let dailyReport = Target.feature(
     name: "DailyReport",
-    bundleIdentifier: bundleIdentifier + ".dailyReport"
+    bundleIdentifier: bundleIdentifier + ".dailyReport",
+    deploymentTarget: deploymentTarget
 )
 
 let settingsDictionary = SettingsDictionary()
     .customCodeSigning()
     .otherSwiftFlags("-Xfrontend -warn-long-function-bodies=300 -Xfrontend -warn-long-expression-type-checking=300")
 
-let project = Project.app(
+let app = Target(
     name: "Covid",
-    bundleIdentifier: bundleIdentifier,
-    settingsDictionary: settingsDictionary,
-    targets: [core, bars, numberR, dailyReport]
+    platform: .iOS,
+    product: .app,
+    bundleId: bundleIdentifier,
+    deploymentTarget: deploymentTarget,
+    infoPlist: .extendingDefault(with: [
+        "CFBundleShortVersionString": "1.0",
+        "CFBundleVersion": "1",
+        "UIMainStoryboardFile": "",
+        "UILaunchStoryboardName": "LaunchScreen",
+        "BGTaskSchedulerPermittedIdentifiers": [
+            "cz.ackee.enterprise.covid.refreshData"
+        ],
+        "UISupportedInterfaceOrientations": [
+            "UIInterfaceOrientationPortrait"
+        ]
+    ]),
+    sources: ["Covid/**"],
+    resources: ["Covid/Resources/**"],
+    dependencies: [core, bars, numberR, dailyReport].map(\.name).map(TargetDependency.target)
+)
+
+let project = Project(
+    name: "Covid",
+    settings: Settings(base: settingsDictionary),
+    targets: [app, core, bars, numberR, dailyReport]
 )
--- Tuist/ProjectDescriptionHelpers/Project+Feature.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Feature.swift
@@ -1,15 +1,17 @@
 import ProjectDescription
 
 public extension Target {
-    static func graph(
+    static func feature(
         name: String,
-        bundleIdentifier: String
+        bundleIdentifier: String,
+        deploymentTarget: DeploymentTarget
     ) -> Self {
         Target(
             name: name,
             platform: .iOS,
             product: .framework,
             bundleId: bundleIdentifier,
+            deploymentTarget: deploymentTarget,
             infoPlist: .default,
             sources: "Features/\(name)/**",
             dependencies: [.target(name: "CovidCore")]
